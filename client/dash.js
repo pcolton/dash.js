cbd3c86d3b7d71024f0491c26a20552021e5d598
Paul Colton <paul+github@colton.net>
2011-05-10 17:39:27 -0700

    support for server-side model syncing, part 1

--- client/dash.js
+++ client/dash.js
@@ -1,10 +1,12 @@
-// Dash.js v0.3.0
+// Dash.js v0.3.1
 // (c) 2011 Paul Colton
 // See LICENSE file for licensing information or 
 // visit http://dashjs.com
 
 (function()
 {
+	var VERSION = '0.3.1';
+
 	var root = this;
 
 	// Function to create a subclass from a base class and provide
@@ -30,7 +32,7 @@
 	var Dash = root.Dash = subclass(Backbone);
 
 	// Current version of the library. Keep in sync with `package.json`.
-	Dash.VERSION = '0.3.0';
+	Dash.VERSION = VERSION;
 
 	// Attached the subclas function to the root object for future use
 	Dash.subclass = subclass;
@@ -56,97 +58,38 @@
 		}
 	}
 
-	// Adapted from backbone-localstorage.js.
-	// The data link is represented by a single JS object.
-	// Create it with a meaningful name, like the name you'd give a table.
-	Dash.dashlink = function(name)
-	{
-		var _this = this;
-		this.name = name;
-
-		// Delayed init 
-		Dash.ready(function() 
-		{
-			var store = Dash.io._dashlinkLoad(_this.name);
-			_this.data = (store && JSON.parse(store)) || {};
-		});
-	}
-
-	// Adapted from backbone-localstorage.js.
-	_.extend(Dash.dashlink.prototype,
-	{
-		save: function()
-		{
-			//console.log("DASHLINK/SAVE: " + this.name + " = " + JSON.stringify(this.data));
-			Dash.io._dashlinkSave(this.name, JSON.stringify(this.data));
-		},
-
-		create: function(model)
-		{
-			if(!model.id) model.id = model.attributes.id = guid();
-			this.data[model.id] = model;
-			this.save();
-
-			//console.log("DASHLINK/CREATE: " + this.name + " , model: " + model.id);
-
-			return model;
-		},
-	
-		update: function(model)
-		{
-			this.data[model.id] = model;
-			this.save();
-
-			// console.log("DASHLINK/UPDATE: " + this.name + " , model: " + model.id);
-
-			return model;
-		},
-
-		find: function(model)
-		{
-			// console.log("DASHLINK/FIND: " + this.name + " , model: " + model.id);
-			return this.data[model.id];
-		},
-
-		findAll: function()
-		{
-			// console.log("DASHLINK/FINDALL: " + this.name);
-			return _.values(this.data);
-		},
-
-		destroy: function(model)
-		{
-			// console.log("DASHLINK/DELETE: " + this.name + " , model: " + model.id);
-			delete this.data[model.id];
-			this.save();
-			return model;
-		}
-	});
-
-	// Adapted from backbone-localstorage.js.
-	// Override Backbone.sync to use delegate to the model or collection's 
-	// dashlink property, which should be an instance of Dash.dashlink.
+	// Override Backbone.sync to use Dash.io for server syncing.
 	Dash.__super__.sync = function(method, model, success, error)
 	{
-		var resp;
-		var link = model.dashlink || model.collection.dashlink;
+		var data;
 
-		switch (method)
+		switch(method)
 		{
-			case "read":    resp = model.id ? link.find(model) : link.findAll(); break;
-			case "create":  resp = link.create(model);                            break;
-			case "update":  resp = link.update(model);                            break;
-			case "delete":  resp = link.destroy(model);                           break;
+			case "read":    
+			case "delete":  
+				var type;
+				if(model instanceof Dash.Collection) type = model.model.prototype.type;
+				else type = model.type;
+
+				
+				data = type + ":" + (model.id ? model.id : '*');
+				break;
+			case "create":  
+			case "update":  
+				data = JSON.stringify(model.toJSON());
+				break;
 		}
 
-		if (resp)
+		Dash.io._dashlink(method, data, function(result)
 		{
-			success(resp);
-		}
-		else
-		{
-			error("Record not found");
-		}
+			if(result)
+			{
+				success(result);
+			}
+			else
+			{	error("Record not found");
+			}
+		});
 	}
 
 	// Setup the Dash.io object and load remote templates
